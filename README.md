76770e3336cd5cc6a4464159ffbaa10c00d8625a
Stephen Schrauger <stephen@schrauger.com>
2017-06-13 11:02:56 -0400

    first version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # slickdeals-hide-expired-contests
 Slickdeals - Hide expired contests
+
+## Installation Instructions
+
+The easiest way to install the script is to first have [GreaseMonkey][greasemonkey] ([Firefox][gm_firefox]) or [TamperMonkey][tampermonkey] ([Chrome][tm_chrome], [Safari][tm_safari], [Opera][tm_opera]). If you have those addons installed already, simply [open the script][script] and follow the prompts to install it.
+
+For more detailed steps, [follow these instructions][instructions] for your particular browser.
+
+[greasemonkey]: http://www.greasespot.net/
+[gm_firefox]: https://addons.mozilla.org/en-us/firefox/addon/greasemonkey/
+[tampermonkey]: https://tampermonkey.net/index.php
+[tm_chrome]: https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo
+[tm_safari]: https://tampermonkey.net/index.php?ext=dhdg&browser=safari
+[tm_opera]: https://addons.opera.com/en/extensions/details/tampermonkey-beta/
+[script]: https://raw.githubusercontent.com/schrauger/slickdeals-hide-expired-contests/master/slickdeals-hide-expired-contests.user.js
+[instructions]: http://stackapps.com/tags/script/info
---
+++ slickdeals-hide-expired-contests.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name        Slickdeals - Hide expired contests
+// @namespace   com.schrauger.slickdeals.contests
+// @author      Stephen Schrauger
+// @description Using dates in the title, it hides any contests whose end date has already passed.
+// @homepage    https://github.com/schrauger/slickdeals-hide-expired-contests
+// @include     https://slickdeals.net/forums/forumdisplay.php?f=25*
+// @version     1.0
+// @grant       none
+// @downloadURL https://raw.githubusercontent.com/schrauger/slickdeals-hide-expired-contests/master/slickdeals-hide-expired-contests.user.js
+// @updateURL   https://raw.githubusercontent.com/schrauger/slickdeals-hide-expired-contests/master/slickdeals-hide-expired-contests.user.js
+// ==/UserScript==
+$(document).ready(function() {
+
+   $('.threadtitleline > a').each(function(){
+    var contest_date_text = $(this).text().match(/\d{1,2}([\/.-])\d{1,2}(\1\d{0,4})?/g);
+    if (!contest_date_text) {
+      // no date detected
+      return true; // date is unknown, so it isn't known to be in the past.
+    }
+
+    if ((String(contest_date_text).match(/[\/.-]/g) || []).length <= 1){
+      // no year provided. add that on
+      contest_date_text = contest_date_text + "/2017";
+    }
+
+    var contest_date = new Date(contest_date_text);
+    var today_date = new Date();
+    today_date.setHours(0,0,0,0);
+        //console.log(contest_date_text + ' ' + contest_date + ' ' +  today_date + (contest_date < today_date));
+    if (contest_date < today_date){
+      $(this).closest('tr').hide();
+      //console.log('in the past');
+    }
+  });
+});
+
